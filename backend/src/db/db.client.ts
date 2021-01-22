2dc1fbf8d2d48d227b6552f580dd6ead24de982f
Bartek-Figat <figat29@gmail.com>
2021-01-21 23:48:31 -0800

    improve db connection

--- backend/src/db/db.client.ts
+++ backend/src/db/db.client.ts
@@ -1,20 +1,31 @@
 require("dotenv").config();
 const { MongoClient } = require("mongodb");
+import logger from '../util/winston.createLogger'
 
-const { dbURI } = process.env;
 
-const dbOptions = {
+const { dbURI } =  process.env as {
+    [key: string]: string;
+  };
+
+
+
+interface dbOptionsType {
+    useUnifiedTopology: boolean;
+    useNewUrlParser: boolean;
+}
+
+const dbOptions: dbOptionsType = {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 };
 
 const client = new MongoClient(dbURI, dbOptions);
 
-module.exports = async () => {
+module.exports = async (): Promise<any> => {
  try {
     await client.connect();
     await client.db("admin").command({ ping: 1 });
-    console.log("Connected successfully to Client DB");
+    logger.info(`DB connection`, {message: `Connected successfully to Client DB`} );
   } finally {
     await client.close();
   }
