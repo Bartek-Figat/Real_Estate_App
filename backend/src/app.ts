f3897f6557eb0d966155dfa0455f45afc4239f1d
Bartek-Figat <figat29@gmail.com>
2021-02-06 17:20:13 -0800

    cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -16,7 +16,7 @@ const initialize = () => {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
   app.use(compression());
-  app.use(cors());
+  app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
   app.use(helmet());
   app.use('/docs', swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
     return res.send(swaggerUi.generateHTML(await import('../build/swagger.json')));
